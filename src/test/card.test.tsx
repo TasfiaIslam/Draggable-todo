1ee6ee9da35a22c6d41a1c6d7a76f9e9f4d2ab4b
TasfiaIslam <tasf1a027@gmail.com>
2023-05-17 23:26:17 +0600

    update test

--- src/test/card.test.tsx
+++ src/test/card.test.tsx
@@ -3,22 +3,24 @@ import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
-it("renders card with expected props", async () => {
-  // Render the component
-  render(
-    <DragDropContext onDragEnd={() => {}}>
-      <Droppable droppableId={"droppableTestId"} key={"droppableTestId"}>
-        {(provided) => (
-          <div {...provided.droppableProps} ref={provided.innerRef}>
-            <Card title="title" id={1} draggableId="testId" index={1} />
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
-    </DragDropContext>
-  );
-  const headingElement = screen.getByText("title");
+describe("#Card", () => {
+  it("renders card component with expected props", async () => {
+    // Render the component
+    render(
+      <DragDropContext onDragEnd={() => {}}>
+        <Droppable droppableId={"droppableTestId"} key={"droppableTestId"}>
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              <Card title="title" id={1} draggableId="testId" index={1} />
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    );
+    const headingElement = screen.getByText("title");
 
-  // Assert that the rendered prop matches the expected value
-  expect(headingElement).toBeDefined();
+    // Assert that the rendered prop matches the expected value
+    expect(headingElement).toBeDefined();
+  });
 });
